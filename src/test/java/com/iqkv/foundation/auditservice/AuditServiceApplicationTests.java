62b877e61773fef5470d88d98b2bb0eb3d22b17a
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-07-06 17:29:12 +0300

    test: enable test profile for integration test

--- src/test/java/com/iqkv/foundation/auditservice/AuditServiceApplicationTests.java
+++ src/test/java/com/iqkv/foundation/auditservice/AuditServiceApplicationTests.java
@@ -18,8 +18,10 @@ package com.iqkv.foundation.auditservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuditServiceApplicationTests {
 
   @Test
